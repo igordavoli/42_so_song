a49219029a2dda0b6e4278255b94e5a54165232b
Igor Davoli <igordavoli@ioasys.com.br>
2022-01-20 06:33:45 -0300

    put map on window

---
+++ src/ft_put_map.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_put_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idavoli- <idavoli-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 05:44:16 by idavoli-          #+#    #+#             */
+/*   Updated: 2022/01/20 06:28:25 by idavoli-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+void	ft_put_sprite(t_game *_game, char c, int x, int y)
+{
+	t_game	*game;
+
+	game = (t_game *) _game;
+	if (c == '0')
+		mlx_put_image_to_window(game->mlx, game->win, game->grass.ref, x * game->grass.width, y * game->grass.height);
+	if (c == '1')
+		mlx_put_image_to_window(game->mlx, game->win, game->wall.ref, x * game->grass.width, y * game->grass.height);
+	if (c == 'P')
+	{
+		mlx_put_image_to_window(game->mlx, game->win, game->hero.ref, x * game->hero.width, y * game->hero.height);
+		game->hero.x = x * game->hero.width;
+		game->hero.y = y * game->hero.height;
+	}
+}
+
+void	ft_put_map(t_game *_game)
+{
+	char	**map;
+	t_game	*game;
+	int		i;
+	int		j;
+
+	game = (t_game *)_game;
+	map = game->map.mtx;
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			ft_put_sprite(game, map[i][j], j, i);
+			j++;
+		}
+		i++;
+	}
+}
